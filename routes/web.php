2466ef6e30611026c696cc86c388d0b3f0ec65a4
John Rhey Bocalan <jrboc18@gmail.com>
2021-03-20 19:43:38 +0800

    Removed extra auth links and files. Moved login and registration to pages folder

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [Controllers\Auth\LoginController::class, 'redirectToLoginForm']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
+Auth::routes([
+    'verify' => false,
+    'reset' => false,
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
